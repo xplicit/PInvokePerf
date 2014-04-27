2b4ae4a99d8a61feaa1bdfc3fefb767b3f2d99f5
xplicit <svg@ngs.ru>
2014-04-28 05:41:16 +0700

    Added workaround for InternalCall Now it does not throw 'MissingMethodException' even in non-AOT mode

--- PInvokePerf/Program.cs
+++ PInvokePerf/Program.cs
@@ -7,19 +7,43 @@ namespace PInvokePerf
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
-		{
-			InitArray ();
-			PerformanceTest.InitInternals ();
-			Console.WriteLine ("Performance measuring starting");
-			Performance ();
-		}
+		delegate void HideFromJit();
 
+		const int nTests=100000;
 		const int maxElems=100;
 		const int maxChars=100;
 		static string[] perfArray = new string[maxElems];
 		static byte[] testBytes = new byte[16384];
-		const int nTests=100000;
+
+		public static void Main (string[] args)
+		{
+			InitArray ();
+			PerformanceTest.InitInternals ();
+//			GC ();
+			Console.WriteLine ("Performance measuring starting");
+
+			//You can call it directly in AOT mode
+//			for (int i = 0; i < 5; i++) {
+//				Performance ();
+//			}
+
+			//This is workaround for mono bug/feature
+			//When mono started it tries to compile 'Main' and recursively 
+			//all methods, which are called from 'Main' method
+			//And in our case it compiles managed_to_native wrapper for our Internal Call methods 
+			//which are not registred yet by 'InitInternal'. (We are expecting to register icalls
+			//first and only after compile them). In this case JIT cannot find icall entry points,
+			//generates 'MissingMethodException' body for managed_to_native wrapper and cashes it. 
+			//When we register new icall entry later we can't regenerate wrapper or remove it from 
+			//the wrapper's cache. 
+			//To workaround this behaviour we hide our functions from JIT by calling them through
+			//delegate 
+			//By the way in AOT mode you don't need to use such workaround all works out of the box.
+			HideFromJit d=Performance;
+			for (int i = 0; i < 5; i++) {
+				d ();
+			}
+		}
 
 		public static void InitArray()
 		{
@@ -37,10 +61,20 @@ namespace PInvokePerf
 				testBytes [i] = (byte)(i & 0xff);
 		}
 
+		public static void GC()
+		{
+			int sum = 0;
+			for (int n = 0; n < nTests; n++) {
+				for (int i = 0; i < perfArray.Length; i++) {
+					perfArray [i] = "abcdefg"+i.ToString()+n.ToString();
+					int s1 = PerformanceTest.InternalCount (perfArray, i);
+					sum += s1;
+				}
+			}
+		}
+
 		public static void Performance()
 		{
-			PerformanceTest test = new PerformanceTest ();
-
 			int s = PerformanceTest.ManagedCount (perfArray,0);
 			int sum = 0;
 
